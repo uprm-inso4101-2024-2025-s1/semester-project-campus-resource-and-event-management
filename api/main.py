962b7e60d8936b2ec3466f1253ef345c6eff6fb3
Ojani Figueroa <ojanifigueroa.of@gmail.com>
2024-09-08 10:25:22 -0400

    Added flask python main file and a test http request from the frontend to the backend.

---
+++ api/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, make_response
+
+app = Flask(__name__)
+
+class reqCounter():
+    def __init__(self):
+        self.count = 0
+    
+    def getCount(self):
+        self.count += 1
+        return self.count
+    
+reqCount = reqCounter()
+
+@app.route("/api/")
+def hello_world():
+    resp = make_response(f"This is the server request # {reqCount.getCount()}", 200)
+
+    # Setting CORS request headers
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -3,8 +3,37 @@
   const increment = () => {
     count += 1
   }
+
+  let serverData
+  const getServerData = async () => {
+    const response = await fetch('http://127.0.0.1:3451/api/')
+    const textResponse = await response.text()
+    serverData = textResponse
+  }
 </script>
 
 <button on:click={increment}>
   count is {count}
 </button>
+
+<button on:click={getServerData}>
+  Get Data From Server
+</button>
+
+{#if serverData}
+  <div>Fetched data: <span>{serverData}</span></div>
+{/if}
+
+<style>
+  div {
+    transform: translateX(-50%);
+    position: absolute;
+    margin-top: 0.5em;
+    font-size: 1.2em;
+    left: 50%;
+  }
+
+  span {
+    color: green;
+  }
+</style>
