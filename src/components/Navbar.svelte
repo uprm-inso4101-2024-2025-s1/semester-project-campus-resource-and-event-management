adc15af87ba78fe97f5f77bf7894129febe5efcc
Jean P. I. Sánchez <jeanpiere.sanchez@upr.edu>
2024-09-17 00:12:07 -0400

    Corrected a style settings bug

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -19,3 +19,24 @@
 </nav>
 
 
+
+
+
+<style>
+    nav {
+      justify-content: space-around;
+      background: #EFEFEF;
+      display: flex;
+      padding: 2em;
+    }
+  
+    a {
+    font-weight: 500;
+    color: #646cff;
+    text-decoration: inherit;
+  }
+  
+    a:hover {
+      color: #535bf2;
+    }
+  </style>
--- src/components/PageWrapper.svelte
+++ src/components/PageWrapper.svelte
@@ -8,26 +8,9 @@
     e.preventDefault();
     window.history.replaceState(null, '', e.target.pathname);
   }
-  
+
 </script>
 
 <Navbar/>
 
-<style>
-  nav {
-    justify-content: space-around;
-    background: #EFEFEF;
-    display: flex;
-    padding: 2em;
-  }
-
-  a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-  a:hover {
-    color: #535bf2;
-  }
-</style>
+<slot />
