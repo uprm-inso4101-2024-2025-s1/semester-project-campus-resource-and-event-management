d53adcf7165e28449e02930de7d9b2adccabe574
LuisJCruz <luis.cruz103@upr.edu>
2024-09-18 15:24:19 -0400

    initial footer

--- src/components/HomePage.svelte
+++ src/components/HomePage.svelte
@@ -1,20 +1,32 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
 <footer>
-
-<!-- Put the footer implementation HERE -->
-
+    <p>&copy; 2024 Eventero. All rights reserved.</p>
+    <nav>
+      <ul>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Terms of Service</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+      </ul>
+    </nav>
 </footer>
+
+<style>
+    footer {
+    background-color: #053A2D;
+    padding: 20px;
+    text-align: center;
+  }
+
+
+  footer nav ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    font-weight: 500;
+    color: #000000;
+    text-decoration: inherit;
+    margin: 0 10px;
+  }
+</style>
 
 
