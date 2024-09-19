602160d872692cb08e1c10968887f8adf03713a8
Carlos Hernandez <carlos.hernandez43@upr.edu>
2024-09-19 16:59:46 -0400

    Ribbon Component Day, Week and Month CalendarPage

---
+++ src/components/CalendarPage.svelte
@@ -0,0 +1,7 @@
+<script>
+    import RibbonComponent from './RibbonCompenent.svelte';
+   </script>
+<body>
+    <RibbonComponent/>
+</body>
+  
---
+++ src/components/RibbonCompenent.svelte
@@ -0,0 +1,33 @@
+<style>
+    .Menu_bar{
+        background-color: white;
+        width: 99%;
+        height: 50%;
+        display:flex; 
+        justify-content:flex-end;
+        border-radius: 15px;
+        margin: 0 10px 0 10px;
+        gap: 34px;
+        
+    }
+    .Calendar_Selection{
+        margin-right: 80px;
+    }
+    p{
+        color: black;
+    }
+    p:hover{
+        color: green;
+    }
+
+</style>
+
+<div class="Menu_bar">
+    
+    <p>Day</p>
+    <p>Week</p>
+    <p class="Calendar_Selection">Month</p>
+    
+
+
+</div>
