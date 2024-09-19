34ada2f4e85963cb2c385b1349881e519bc8f100
AlexisSantiago23 <santiagorivera.alexism@gmail.com>
2024-09-19 18:12:05 -0400

    made_issue_77_78_79_80

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,15 +1,18 @@
 <script>
-
+    import facebookLogo from '../assets/facebook.png';
+    import igLogo from '../assets/instagram.png';
+    import xLogo from '../assets/x.png';
 </script>
+
 <style>
- footer {
+    footer {
         width: 100%;
         height: max-content;
-        background-color: #0C9D7A;
+        background-color: #EFEFEF;
         margin-top: 1rem;
         padding: 2.5rem 0;
         border-radius: 6px;
-        box-shadow: 0px 2px 15px 2px white;
+        /* box-shadow: 0px 2px 15px 2px white; */
     }
     .footer-logo {
         width: 10rem;
@@ -99,8 +102,6 @@
     .footer-addres {
         width: 100%;
     }
-
-
     /* mobile */
     @media (min-width: 320px) and (max-width: 767px) and (orientation: portrait) {
         .footer-contactUs {
@@ -154,94 +155,96 @@
             margin: 26px 0px 19px 10px;
         }
     }
+
 </style>
+
 <footer>
-     <h2></h2>
-    <h3></h3>
-    <p id="footer-address"></p>
-    <h3></h3>
-    <p id="footer-address"></p>
-    <h3></h3>
+    <h2>Contact Us</h2>
+    <h3>Postal Address:</h3>
+    <p id="footer-address">P.O Box 9000 University of Puerto Rico at Mayagüez Calle Post, Mayagüez, P.R. 00681-9000</p>
+    <h3>Fisical Address:</h3>
+    <p id="footer-address">P.O Box 9000 University of Puerto Rico at Mayagüez Calle Post, Mayagüez, P.R. 00681-9000</p>
+    <h3>Main Telephone: (787) 832-4040</h3>
 
     <div class="footer-contactUs">
         <div class="footer-contactUs-categorias">
-            <h4></h4>
+            <h4>Students Services:</h4>
             <div>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
+                <p>Admissions x2400,X2412</p>
+                <p>Dean of Students x3862,3250</p>
+                <p>Registrar's Office x3813, x3284</p>
+                <p>Clinic, Medical Services x3722 Emergency x2323</p>
+                <p>Financial Aid X3863</p>
             </div>
         </div>
 
         <div class="footer-contactUs-categorias">
-            <h4></h4>
+            <h4>Faculties:</h4>
             <div>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
+                <p>College of Business Administration ×3800, x3804</p>
+                <p>College of Agricultural Sciences x3850,x1218</p>
+                <p>College of Arts & Sciences x3828, x3033</p>
+                <p>College of Engineering x3822, x3504</p>
+                <p>Dean of Administration x2020, x2024</p>
+                <p>Dean of Accademic Affairs x3582, x3807</p>
             </div>
         </div>
 
         <div class="footer-contactUs-categorias">
-            <h4>:</h4>
+            <h4>Investigations:</h4>
             <div>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
+                <p>Agricultural Extension Service (787) 265-4130, x3592</p>
+                <p>Agricultural Experiment Station (787) 833-7765, x3899</p>
+                <p>Sea Grant (787) 265-2880, x2165</p>
+                <p>Seismic Network (787) 833-8433, x5452</p>
+                <p>Center for Economical Development (787) 833-4445, x3613</p>
+                <p>Research and Development Center (787) 831-2065, x5250, 5213</p>
             </div>
         </div>
 
         <div class="footer-contactUs-categorias">
-            <h4></h4>
+            <h4>Other Services:</h4>
             <div>
-                <p></p>
-                <p></p>
-                <p></p>
+                <p>Chancellors Office x3131, x3139</p>
+                <p>Press and Communications x3879, x2332</p>
+                <p>Campus Security x3263, x3872, x2458, x3538</p>
             </div>
         </div>
     </div>
         
     <div class="footer-quickLinks">
         <div>
-            <h3></h3>
-            <p> <a href=""></a></p>
+            <h3>Resources:</h3>
+            <p>Link <a href="https://www.uprm.edu/area-e/resources/">here</a></p>
         </div>
         <div>
-            <h3></h3>
-            <p><a href=""></a></p>
+            <h3>Events:</h3>
+            <p>Link <a href="https://www.uprm.edu/portada/">here</a></p>
         </div>
         <div>
-            <h3></h3>
-            <p><a href=""></a></p>
+            <h3>Map:</h3>
+            <p>Link <a href="https://uprm.edu/portales/mapa/">here</a></p>
         </div>
         <div>
-            <h3></h3>
-            <p> <a href=""></a></p>
+            <h3>News:</h3>
+            <p>Link <a href="https://www.uprm.edu/portada/noticias/">here</a></p>
         </div>
         <div>
-            <h3></h3>
-            <p><a href=""></a></p>
+            <h3>FAQ:</h3>
+            <p>Link <a href="https://www.uprm.edu/placement/inicio/faq/">here</a></p>
         </div>
     </div>
 
     <div class="footer-socialMedia">
-        <h4></h4>
+        <h4>Social Media:</h4>
         <div class="footer-socialMedia-logos">
-            <a href=""><img src = "" alt = "facebook" class="footer-logo footer-logo-facebook"></a> 
-            <a href=""><img src = ""alt = "" class="footer-logo footer-logo-facebook"></a>
-            <a ><img src = "" class="footer-logo footer-logo-facebook"></a>
+            <a href="https://www.facebook.com/RecintoUniversitariodeMayaguez/?locale=es_LA"><img src={facebookLogo} alt = "facebook" class="footer-logo footer-logo-facebook"></a> 
+            <a href="https://www.instagram.com/uprmayaguez/"><img src={igLogo} alt = "ig" class="footer-logo footer-logo-facebook"></a>
+            <a href="https://x.com/uprm?lang=es"><img src={xLogo} alt = "x" class="footer-logo footer-logo-facebook"></a>
         </div>
     </div>
     
      <div class="footer-termsLegal">
-        <a href = ""><p  class="footer-terms" ></p></a>
+        <a href="https://www.uprm.edu/portales/terminos-y-condiciones-de-uso/"><p  class="footer-terms" >Terms of Service  and Privacy Policy</p></a>
     </div>
 </footer>
