0b95dad253e65e780c6d80eec30d1982e95d5fe4
KennethSepu1 <124213573+KennethSepu1@users.noreply.github.com>
2024-09-18 23:30:36 -0400

    Added Icons on Homepage with working links

--- src/components/HomePage.svelte
+++ src/components/HomePage.svelte
@@ -1,3 +1,4 @@
+
 <footer>
     <p>&copy; 2024 Eventero. All rights reserved.</p>
     <nav>
@@ -8,6 +9,19 @@
         <li><a href="#">Privacy Policy</a></li>
       </ul>
     </nav>
+
+    <div class="social-icons">
+      <a href="https://www.facebook.com/RecintoUniversitariodeMayaguez" target="_blank">
+        <img src="/images/FB_Logo_white.png" alt="Facebook" class="social-icon" />
+      </a>
+      <a href="https://www.youtube.com/user/videocolegio" target="_blank">
+        <img src="/images/YT_Logo_white.png" alt="YouTube" class="social-icon-youtube" />
+      </a>
+      <a href="https://twitter.com/PrensaRUM" target="_blank">
+        <img src="/images/X_Logo.png" alt="X" class="social-icon" />
+      </a>
+    </div>
+  
 </footer>
 
 <style>
@@ -26,6 +40,22 @@
     color: #000000;
     text-decoration: inherit;
     margin: 0 10px;
+  }
+
+  .social-icon {
+    width: 50px;
+    height: auto;
+    object-fit: contain;
+  }
+
+  .social-icon-youtube {
+    width: 50px; 
+    height: auto;
+    transform: scale(2.5); 
+  }
+
+  .social-icons a {
+    margin: 0 30px;
   }
 </style>
 
