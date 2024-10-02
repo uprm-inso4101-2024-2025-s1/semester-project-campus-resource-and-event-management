4ef7a7d0f9344672b04828ad4c19eb278744655c
marymarmaldonado <marymar.maldonado@upr.edu>
2024-10-02 15:17:55 -0400

    Added title and slogan.

--- src/components/HomePage.svelte
+++ src/components/HomePage.svelte
@@ -70,6 +70,47 @@
   import Footer from "../components/Footer.svelte"
 </script>
 
+
+<section class="title-section">
+  <h1>Campus Resource and Event Manager</h1>
+  <p>Your gateway to campus life.</p>
+</section>
+
+
+
+<style>
+
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+  body {
+      font-family: 'Roboto', sans-serif;
+  }
+
+  .title-section {
+    padding: 40px;
+    background-color: #ffffff;
+  }
+
+  .title-container {
+    margin-left: 200px; /* Adjust based on Figma specs */
+    text-align: left;
+    max-width: 700px; /* Optional, to constrain the width */
+  }
+
+  .title-section h1 {
+    font-size: 40px;
+    font-weight: 700;
+    color: #085E49;
+    margin-bottom: 10px;
+  }
+
+  .title-section p {
+    font-size: 20px;
+    color: #004d40;
+    margin-top: 5px;
+  }
+  
+</style>
+
 <body>
   <Footer/> 
 </body>
