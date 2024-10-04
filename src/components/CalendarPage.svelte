48ada4fa2a62596c5dd12ad0fd3744034d6eab60
Ojani <ojanifigueroa.of@gmail.com>
2024-10-03 22:01:21 -0400

    Added the ribbon to the calendarpage layout and added the functionality to switch between calendar views.

--- src/components/CalendarPage.svelte
+++ src/components/CalendarPage.svelte
@@ -1,31 +1,37 @@
 <script>
     import RibbonComponent from "./RibbonCompenent.svelte";
     import MainCalendar from "./MainCalendar.svelte";
+    let mainCalendarView = 'month'
 </script>
 
 <div class="layout">
     <div class="ribbonContainer">
-        <RibbonComponent />
+        <RibbonComponent bind:mainCalendarView />
     </div>
     <div class="miniCalendarContainer"></div>
     <div class="AddEventSectionContainer"></div>
     <div class="mainCalendarContainer">
-        <MainCalendar />
+        <MainCalendar view={mainCalendarView} />
     </div>
 </div>
 
 <style>
     .layout {
-        grid-template-columns: repeat(60, 1fr);
-        grid-template-rows: repeat(60, 1fr);
+        grid-template-columns: repeat(120, 1fr);
+        grid-template-rows: repeat(120, 1fr);
         background: rgb(223, 223, 223);
         overflow: hidden;
         display: grid;
         height: 100%;
     }
 
+    .ribbonContainer {
+        grid-column: 2 / 120;
+        grid-row: 3 / 10;
+    }
+
     .mainCalendarContainer {
-        grid-column: 20 / 60;
-        grid-row: 6 / 60;
+        grid-column: 40 / 119;
+        grid-row: 12 / 119;
     }
 </style>
--- src/components/RibbonCompenent.svelte
+++ src/components/RibbonCompenent.svelte
@@ -1,33 +1,38 @@
+<script>
+    export let mainCalendarView = 'month'
+</script>
+
+<div class="Menu_bar">
+    <button on:click={() => mainCalendarView = 'day'} class:currentView={mainCalendarView == 'day'}>Day</button>
+    <button on:click={() => mainCalendarView = 'week'} class:currentView={mainCalendarView == 'week'}>Week</button>
+    <button on:click={() => mainCalendarView = 'month'} class:currentView={mainCalendarView == 'month'} class="Calendar_Selection">Month</button>
+</div>
+
 <style>
     .Menu_bar{
         background-color: white;
-        width: 99%;
-        height: 50%;
+        position: relative;
+        width: 100%;
+        height: 100%;
         display:flex; 
+        align-items: center;
         justify-content:flex-end;
-        border-radius: 15px;
-        margin: 0 10px 0 10px;
+        border-radius: 1.2em;
         gap: 34px;
         
     }
     .Calendar_Selection{
         margin-right: 80px;
     }
-    p{
+    button {
+        all: unset;
+        font-weight: bolder;
+        user-select: none;
+        cursor: pointer;
         color: black;
     }
-    p:hover{
-        color: green;
+    .currentView, button:hover{
+        color: rgb(8, 94, 73);
     }
 
 </style>
-
-<div class="Menu_bar">
-    
-    <p>Day</p>
-    <p>Week</p>
-    <p class="Calendar_Selection">Month</p>
-    
-
-
-</div>
