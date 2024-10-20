58ceea4febaed71176e3bc5ea28c603a97a2e7d7
MiguelGibo <miguel.giboyeaux@upr.edu>
2024-10-20 13:40:59 -0400

    Made Setter methods

--- api/event.py
+++ api/event.py
@@ -12,3 +12,27 @@ class event():
         self.__description = description
         self.__tags = tags
 
+    # Setter Methods
+    def setEventID(self, eventID):
+        self.__event_id = eventID
+    def setName(self, name):
+        self.__name = name
+    def setRoomID(self, roomID):
+        self.__roomID = roomID
+    def setOrganizer(self, organizer):
+        self.__organizer = organizer
+    def setAttendees(self, attendees):
+        self.__attendees = attendees
+    def setData(self, data):
+        self.__date = date
+    def setDuration(self, duration):
+        self.__duration = duration
+    def setStatus(self, status):
+        self.__status = status
+    def setCapacity(self, capacity):
+        self.__capacity = capacity
+    def setDescription(self, description):
+        self.__description = description
+    def setTags(self, tags):
+        self.__tags = tags
+
